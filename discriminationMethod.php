3a76da73c5aebf28d922c8f3d8c214b19e5f0da0
WangYunJong <wwjy1222@gmail.com>
2025-11-04 15:24:16 +0900

    판별 함수 정리

---
+++ discriminationMethod.php
@@ -0,0 +1,15 @@
+<?php
+//is_int(); 정수 판별
+//is_float(); 실수 판별
+//is_numeric(); 숫자 판별
+//pi() 파이를 나타냄.
+
+$a = 1;
+
+if(is_int($a)) {
+    print("정수입니다");
+} else {
+    print("정수가 아닙니다.");
+}
+
+?>
